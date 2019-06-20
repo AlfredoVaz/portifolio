1c3abed5a8057d4dd48b077bd11dd3bf36f075a5
Alfredo Vaz <alfredovazn@gmail.com>
2019-06-20 01:54:28 -0300

    Updates

---
+++ precache-manifest.af6c596730e63f7f6c9224d77bccd97a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacde198717866bdf69c32de474a8ee9",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "4fd387b4fc5223d9ae05",
+    "url": "/portifolio/static/css/main.894f4a66.chunk.css"
+  },
+  {
+    "revision": "2785dc34fea951569815",
+    "url": "/portifolio/static/js/2.c2e19106.chunk.js"
+  },
+  {
+    "revision": "4fd387b4fc5223d9ae05",
+    "url": "/portifolio/static/js/main.8e47ba48.chunk.js"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  },
+  {
+    "revision": "68375536e751830b137957aa67ae95f5",
+    "url": "/portifolio/static/media/AlfredoVaz.68375536.pdf"
+  },
+  {
+    "revision": "3ddcacc88d25e504f46e038a91c6e760",
+    "url": "/portifolio/static/media/feitoMelhor.3ddcacc8.png"
+  },
+  {
+    "revision": "d8df97b7a3d5a18ec104c98818acbfdc",
+    "url": "/portifolio/static/media/fight.d8df97b7.jpg"
+  },
+  {
+    "revision": "9efc04175794813688e5e7d94f4d8420",
+    "url": "/portifolio/static/media/geovanaGarcia.9efc0417.JPG"
+  },
+  {
+    "revision": "a3cee4ff92c376832548bfe49f47c173",
+    "url": "/portifolio/static/media/girlgif.a3cee4ff.gif"
+  },
+  {
+    "revision": "b7536715cb00cf9fd25d23c3cc24c5db",
+    "url": "/portifolio/static/media/minhaFoto.b7536715.JPG"
+  },
+  {
+    "revision": "8907e9b65bb7ab9043c2c70adb494b11",
+    "url": "/portifolio/static/media/videoMaker.8907e9b6.jpg"
+  }
+]);
